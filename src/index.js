d9b1e7c9048a311ea419b1b3550c3da2637974f1
venguraserg <venguraserg@gmail.com>
2023-08-01 10:20:03 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+
+    let strNumber = String(Math.abs(n));
+    let newStr='';
+    let reverseNumb;
+    for(let i=strNumber.length -1;i>=0; i--){
+      newStr+=strNumber[i];
+      
+    }
+    
+    return Number(newStr);
 }
+
+
+
+
